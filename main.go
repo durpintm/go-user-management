e3d8523c593fbe2d3cb04b9d780cdb261a6cacb2
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-28 18:28:55 -0400

    Register user added

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 const (
@@ -36,8 +39,36 @@ func setupDatabase() *sql.DB {
 	if(err != nil){
 	log.Fatal(err)
 	}
-	
+
 	fmt.Println("Successfully connected to the database")
 
 	return db
+}
+
+func registerHandler(db *sql.DB) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		if r.Method != "POST"{
+			http.Error(w, "Only POST method is allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+		if err != nil {
+			http.Error(w, "Failed to hash password!", http.StatusInternalServerError)
+			return
+		}
+
+		_, err = db.Exec("INSERT INTO Users (username, password_hash) VALUES ($1, $2)", username, string(hashedPassword))
+
+		if err != nil {
+				http.Error(w, "Failed to save user!", http.StatusInternalServerError)
+				return
+		}
+
+		fmt.Fprintf(w, "User registered successfully!")
+	}
 }
